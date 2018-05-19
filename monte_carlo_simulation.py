8828c98d87fa84c75fec0afc8a52873fa1fc08e9
UmarAhmed <wspuma@gmail.com>
2018-05-19 19:08:16 -0400

    more improvements

--- monte_carlo_simulation.py
+++ monte_carlo_simulation.py
@@ -6,14 +6,13 @@ snakes_and_ladders = {98:78, 95:75, 93:73, 87:24, 64:60, 62:10, 56:53, 49:11, 48
 max_turns_per_game = 1000
 
 
-# Simulates a single player game
-# What is a??
+# Simulates a single player game and returns how long it lasted
 def play():
     position = 0
     turns = 0
     while position < 100 and turns < max_turns_per_game:
         position += random.randint(1, 6)
-        # Check for snakes and ladders
+     # Check for snakes and ladders
     if position in snakes_and_ladders:
             position = snakes_and_ladders[position]
     turns += 1
@@ -26,15 +25,12 @@ def get_data(n):
     data = [play() for i in range(n)]
     freq_count = [i/100 for i in collections.Counter(data.sort())]
     return counter.items()
-    
-data = get_data(10000)     
 
 
-plt.title('Percent chance of finishing a game in n-moves')
-plt.xlabel('Number of Moves')
-plt.ylabel('Percent Chance')
-plt.plot(*zip(*data))
-plt.show()
-
-
-plt.hist(data, cumulative=True)
+def make_table(data):
+    plt.title('Percent chance of finishing a game in n-moves')
+    plt.xlabel('Number of Moves')
+    plt.ylabel('Percent Chance')
+    plt.plot(*zip(*data))
+    plt.show()
+    plt.hist(data, cumulative=True)
